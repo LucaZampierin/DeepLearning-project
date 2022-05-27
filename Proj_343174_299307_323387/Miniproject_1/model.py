8e99dadb25253a90069dc718f02a1a2b17c0be7f
Sawii00 <s.nasturzio@libero.it>
2022-05-27 13:58:08 +0200

    Repo Restructuring

--- Proj_343174_299307_323387/Miniproject_1/model.py
+++ Proj_343174_299307_323387/Miniproject_1/model.py
@@ -2,17 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class bn_lrelu(nn.Module):
-    def __init__(self, input_ch, activation="Relu"):
+    def __init__(self, input_ch):
         super().__init__()
         self.bn = nn.BatchNorm2d(input_ch)
-        if activation == "Relu":
-            self.relu = nn.ReLU()
-        elif activation == "Leaky_Relu":
-            self.relu = nn.LeakyReLU(negative_slope=0.1)
-        elif activation == "PRelu":
-            self.relu = nn.PReLU()
+        self.relu = nn.ReLU()
 
     def forward(self, inputs):
         x = self.bn(inputs)
@@ -20,7 +14,7 @@ class bn_lrelu(nn.Module):
         return x
 
 class DnCNN(torch.nn.Module):
-    def __init__(self, activation="PRelu", encoder_ch=32):
+    def __init__(self):
         super().__init__()
         """Encoder 1"""
         self.conv1 = nn.Conv2d(3, 64, (3, 3), padding='same')
@@ -58,23 +52,24 @@ class DnCNN(torch.nn.Module):
 class Model():
     def __init__(self):
         super().__init__()
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device('cpu' if torch.cuda.is_available() else 'cpu')
         self.model = DnCNN().to(device=self.device)
-        self.optimizer = torch.optim.Adam(params= self.model.parameters(), lr=0.003535281772231024, betas=(0.88, 0.999), weight_decay=4.936803349899099e-07)
+        self.optimizer = torch.optim.Adam(params= self.model.parameters(), lr=0.0031, betas=(0.875, 0.991), weight_decay=0.0000016800238576037557)
         self.criterion = nn.MSELoss().to(device=self.device)
 
     def predict(self, test_input):
         test_input = test_input.float().to(self.device)
         self.model.eval()
         with torch.no_grad():
+            # Self-ensamble
             output = torch.zeros(test_input.shape, device=self.device, dtype=torch.float)
+            # First rotate the current image
             for i in range(4):
-                # output += self.model(test_input.rot90(i, [2, 3]).view(-1, 3, 32, 32)).rot90(-i, [2, 3])
                 output += self.model(test_input.rot90(i, [2, 3]).view(test_input.shape)).rot90(-i, [2, 3])
-
+            # Then flip the given image (mirrored version) and then rotate this flipped version
             for i in range(4):
-                # noisy_val_filp = input.flip(3)
                 output += self.model(test_input.flip(3).rot90(i, [2, 3]).view(test_input.shape)).rot90(-i, [2, 3]).flip(3)
+            # Average the results of the inverse transformed outputs
             output = output / 8
 
         return output.clamp(min=0, max=255)
@@ -87,7 +82,7 @@ class Model():
     def train(self , train_input , train_target , num_epochs=10):
         mini_batch_size = 50
         self.model.train()
-        # train_input, train_target = train_input.float().to(device), train_target.float().to(device)
+        train_input, train_target = train_input.float().to(self.device), train_target.float().to(self.device)
         for e in range(num_epochs):
             for b in range(0, train_input.size(0), mini_batch_size):
                 output = self.model(train_input.narrow(0, b, mini_batch_size).float().to(self.device))
